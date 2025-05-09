e8905dd61a3e5676f6da2c05919a1531bfe511ea
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-05-10 04:53:42 +0530

    fix: modified the layout and matched the dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +23,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+       >
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
