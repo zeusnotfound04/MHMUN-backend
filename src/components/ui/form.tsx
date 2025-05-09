03ad96783f7de6705da7a051bf2a9e6a6300b437
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-05-10 04:35:07 +0530

    fix: modified the form and matched the dark theme

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -80,7 +80,7 @@ function FormItem({ className, ...props }: React.ComponentProps<"div">) {
     <FormItemContext.Provider value={{ id }}>
       <div
         data-slot="form-item"
-        className={cn("grid gap-2", className)}
+        className={cn("grid gap-2 mb-4", className)}
         {...props}
       />
     </FormItemContext.Provider>
@@ -129,7 +129,7 @@ function FormDescription({ className, ...props }: React.ComponentProps<"p">) {
     <p
       data-slot="form-description"
       id={formDescriptionId}
-      className={cn("text-muted-foreground text-sm", className)}
+      className={cn("text-indigo-300/70 text-xs italic", className)}
       {...props}
     />
   )
@@ -147,7 +147,7 @@ function FormMessage({ className, ...props }: React.ComponentProps<"p">) {
     <p
       data-slot="form-message"
       id={formMessageId}
-      className={cn("text-destructive text-sm", className)}
+      className={cn("text-red-400 text-sm font-medium", className)}
       {...props}
     >
       {body}
