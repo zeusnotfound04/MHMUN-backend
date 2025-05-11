6f94a38900371d698ef6381b6cba9a453484eabb
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-05-12 01:55:31 +0530

    feat: added s3 url in the next config to render img tthrough this url

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+       {
+        protocol: "https",
+        hostname: "mhmun-2025.s3.ap-south-1.amazonaws.com",
+        pathname: "/**",
+      },
       {
         protocol: "https",
         hostname: "w05m6ci7vu.ufs.sh",
