46364c32f7771d35050c59d1e1df5b728bf69d65
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-05-11 16:38:49 +0530

    fix : updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "w05m6ci7vu.ufs.sh",
+        pathname: "/**",
+      },
+
+    ],
+  },
 };
 
 export default nextConfig;
+
