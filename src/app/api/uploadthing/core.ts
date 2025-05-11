48df94448975fe2c2cfdd09759d2f74886451dad
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-05-11 14:45:51 +0530

    fix : fixed the upload thing eslint issuee

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
